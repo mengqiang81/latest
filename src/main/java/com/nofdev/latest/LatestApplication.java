4ec3bbe03cc998df2e4dcd4640d37f93705e726a
unknown <kingdee@ZGHLWJR-C-2407-0002.kingdee.gbl>
2025-09-02 13:45:16 +0800

    init from spring initializr

---
+++ src/main/java/com/nofdev/latest/LatestApplication.java
@@ -0,0 +1,13 @@
+package com.nofdev.latest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LatestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LatestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nofdev/latest/LatestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nofdev.latest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LatestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
