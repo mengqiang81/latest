53932aabe6cc9953d043c11b831181377f66ddc7
mengqiang <mengqiang81@gmail.com>
2025-09-02 16:24:23 +0800

    我现在得到了一个可以run的分层架构项目

--- starter/src/main/java/com/nofdev/LatestApplication.java
+++ starter/src/main/java/com/nofdev/LatestApplication.java
@@ -1,4 +1,4 @@
-package com.nofdev.latest;
+package com.nofdev;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class LatestApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(LatestApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(LatestApplication.class, args);
+    }
 
 }
--- starter/src/test/java/com/nofdev/LatestApplicationTests.java
+++ starter/src/test/java/com/nofdev/LatestApplicationTests.java
@@ -1,4 +1,4 @@
-package com.nofdev.latest;
+package com.nofdev;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
